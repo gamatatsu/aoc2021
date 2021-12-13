3ff1c64f70faebcb307070e0177b493ee6c5d101
gamatatsu <33262512+gamatatsu@users.noreply.github.com>
2021-12-12 21:41:19 -0600

    dumb

--- day9.py
+++ day9.py
@@ -9,8 +9,46 @@ for i in range(len(arr)):
         arr2[i][digit] = int(char[digit])
 
 print(arr2)
+print(arr2.size)
 
 lowPoints = []
 
+width = len(arr2[0]) - 1
+height = len(arr2) - 1
+
 for x in range(len(arr2[0])):
     for y in range(len(arr2)):
+        point = arr2[y, x]
+        print(x, y)
+        if x == 0 and y == 0:
+            if arr2[y, x + 1] > point and arr2[y - 1, x] > point:
+                lowPoints.append(point)
+        elif x == 0 and y == height:
+            if arr2[y, x + 1] > point and arr2[y - 1, x] > point:
+                lowPoints.append(point)
+        elif x == width and y == 0:
+            if arr2[y, x - 1] > point and arr2[y + 1, x] > point:
+                lowPoints.append(point)
+        elif x == width and y == height:
+            if arr2[y, x - 1] > point and arr2[y - 1, x] > point:
+                lowPoints.append(point)
+        elif x == 0:
+            if arr2[y + 1, x] > point and arr2[y - 1, x] > point and arr2[y, x + 1] > point:
+                lowPoints.append(point)
+        elif y == 0:
+            if arr2[y, x + 1] > point and arr2[y, x - 1] > point and arr2[y - 1, x] > point:
+                lowPoints.append(point)
+        elif x == width:
+            if arr2[y + 1, x] > point and arr2[y - 1, x] > point and arr2[y, x - 1] > point:
+                lowPoints.append(point)
+        elif y == height:
+            if arr2[y, x + 1] > point and arr2[y, x - 1] > point and arr2[y - 1, x] > point:
+                lowPoints.append(point)
+        else:
+            if arr2[y, x + 1] > point and arr2[y, x - 1] > point and arr2[y - 1, x] > point and arr2[y + 1, x] > point:
+                lowPoints.append(point)
+
+print(lowPoints)
+for i in range(len(lowPoints)):
+    lowPoints[i] += 1
+print(sum(lowPoints))
